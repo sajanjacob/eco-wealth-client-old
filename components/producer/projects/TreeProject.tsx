a9f143b4474bc4b2e5eeeb510c99557e4cb79bed
ecoappsandautomation <sajan@ecoappsandautomation.com>
2023-06-24 10:43:00 -0600

    Added totalNumOfInvestors and totalAmountRaised

--- components/producer/projects/TreeProject.tsx
+++ components/producer/projects/TreeProject.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import supabase from "@/utils/supabaseClient";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 type Props = {
 	project: TreeProject | null | undefined;
+	treeInvestments: [] | null | undefined;
 };
 
-export default function TreeProject({ project }: Props) {
+export default function TreeProject({ project, treeInvestments }: Props) {
 	return (
 		<div>
 			<p>Tree Project Type: {project?.type}</p>
-			<p>Target Trees: {project?.treeTarget}</p>
-			<p>Planted Trees: {project?.treeCount}</p>
+			<p>Target Trees: {project?.treeTarget?.toLocaleString()}</p>
+			<p>Planted Trees: {project?.treeCount?.toLocaleString()}</p>
 			<p>Funds requested per tree: ${project?.fundsRequestedPerTree}</p>
 			<p>
 				Total funds requested: $
@@ -18,6 +21,12 @@ export default function TreeProject({ project }: Props) {
 						project?.treeTarget * project?.fundsRequestedPerTree
 					).toLocaleString()}
 			</p>
+			<p>
+				Total Number of Investments:{" "}
+				{treeInvestments?.length ? treeInvestments?.length : 0}
+			</p>
+			<p>Total Number of Investors: {project?.totalNumberOfInvestors}</p>
+			<p>Total Funds Raised: ${project?.totalAmountRaised}</p>
 		</div>
 	);
 }
