0c6c56b7766efbe9da7c0595d69882366742d063
ecoappsandautomation <sajan@ecoappsandautomation.com>
2023-12-02 13:56:31 -0700

    Refactor portfolio page component

--- app/i/portfolio/page.tsx
+++ app/i/portfolio/page.tsx
@@ -6,33 +6,38 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import getBasePath from "@/lib/getBasePath";
 import { useAppSelector } from "@/redux/hooks";
-import { set } from "react-hook-form";
+import Link from "next/link";
+import Loading from "@/components/Loading";
 
 type Props = {};
 
 function Portfolio({}: Props) {
 	const [projects, setProjects] = useState<any>([]);
 	const [loading, setLoading] = useState<boolean>(true);
-	const [totalShares, setTotalShares] = useState<number>(0);
-	const [totalAmountInvested, setTotalAmountInvested] = useState<number>(0);
+	const [totalShares, setTotalShares] = useState<any>([]);
+	const [totalAmountInvested, setTotalAmountInvested] = useState<any>([]);
 	const user = useAppSelector((state) => state.user);
-	// axios call to get user portfolio
+	// axios call to get investor portfolio
 	const fetchUserPortfolio = async () => {
 		const res = await axios.get(
 			`${getBasePath()}/api/investor/portfolio?investorId=${user.investorId}`
 		);
-
-		if (res) {
+		console.log("res >>> ", res);
+		if (res.data.data.length > 0) {
 			console.log("res.data >>> ", res.data);
-			setProjects([convertToCamelCase(res.data.data[0])]);
+
+			setProjects(convertToCamelCase(res.data.data));
 			setTotalShares(res.data.totalShares);
 			setTotalAmountInvested(res.data.totalAmountInvested);
 			setLoading(false);
+			return;
 		}
 		if (res.status === (400 || 401)) {
 			console.log("error fetching investor portfolio projects");
 			setLoading(false);
+			return;
 		}
+		setLoading(false);
 	};
 
 	useEffect(() => {
@@ -44,30 +49,48 @@ function Portfolio({}: Props) {
 	console.log("projects >>> ", projects);
 	if (loading)
 		return (
-			<div>
-				<h1>Your Portfolio</h1>
-				<p>loading...</p>
+			<div className='lg:w-[1200px] mx-auto w-[90%]'>
+				<h1 className='text-3xl font-bold mt-8'>Your Portfolio</h1>
+				<Loading />
+			</div>
+		);
+	if (!loading && projects.length === 0)
+		return (
+			<div className='lg:w-[1200px] mx-auto w-[90%]'>
+				<h1 className='text-3xl font-bold mt-8 ml-8 lg:ml-0'>Your Portfolio</h1>
+				<div className='flex flex-col items-center justify-center h-[80vh]'>
+					<h3 className='text-2xl font-bold mb-4'>
+						No projects found in your portfolio yet.
+					</h3>
+					<p className='text-lg text-center'>Ready to invest into a project?</p>
+					<p className='mt-2 font-bold text-center'>
+						<Link
+							href='/i/discover'
+							className='text-[var(--cta-one)] hover:text-[var(--cta-two-hover)] transition-all cursor-pointer'
+						>
+							Head over to the project discovery page
+						</Link>{" "}
+						to find a project to <br />
+						contribute to.
+					</p>
+				</div>
 			</div>
 		);
 	return (
-		<div>
-			<h1>Your Portfolio</h1>
-			<div>
+		<div className='w-[90%] lg:w-3/4 mx-auto py-8 h-[100%]'>
+			<h1 className='text-3xl font-bold md:mt-8'>Your Portfolio</h1>
+			<div className='flex flex-wrap'>
 				{projects &&
 					projects.length > 0 &&
-					projects.map((project: any) => {
+					projects.map((project: any, index: number) => {
+						console.log("project >>> ", project);
+						console.log("index >>> ", index);
 						return (
 							<PortfolioCard
 								project={project}
 								key={project.id}
-								totalShares={totalShares}
-								totalAmountInvested={totalAmountInvested}
-								totalAmountRaised={
-									project.projectFinancials[0].totalAmountRaised
-								}
-								amountRequested={
-									project.projectFinancials[0].finalEstProjectFundRequestTotal
-								}
+								totalShares={totalShares[index]}
+								totalAmountInvested={totalAmountInvested[index]}
 							/>
 						);
 					})}
