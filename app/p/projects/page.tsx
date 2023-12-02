4bc4c0ab3652b7a7583092e71ff5313579797885
ecoappsandautomation <sajan@ecoappsandautomation.com>
2023-12-02 13:57:01 -0700

    Add loading state and calculate percentages funded

--- app/p/projects/page.tsx
+++ app/p/projects/page.tsx
@@ -7,7 +7,8 @@ import { useAppSelector } from "@/redux/hooks";
 import convertToCamelCase from "@/utils/convertToCamelCase";
 import { useRouter } from "next/navigation";
 import withAuth from "@/utils/withAuth";
-
+import { IoMdAddCircle } from "react-icons/io";
+import Loading from "@/components/Loading";
 type Props = {};
 function Projects({}: Props) {
 	const router = useRouter();
@@ -15,19 +16,25 @@ function Projects({}: Props) {
 	const [projects, setProjects] = useState<
 		Project[] | TreeProject[] | EnergyProject[] | SolarProject[]
 	>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [percentagesFunded, setPercentagesFunded] = useState<any>([]);
 
 	const fetchProjects = async () => {
+		setLoading(true);
 		const { data, error } = await supabase
 			.from("projects")
 			.select(
-				"*, project_milestones(*), tree_projects(*), energy_projects(*), solar_projects(*)"
+				"*, project_milestones(*), tree_projects(*), energy_projects(*), solar_projects(*), project_financials(*)"
 			)
 			.eq("producer_id", user.producerId)
-			.neq("is_deleted", true);
+			.neq("is_deleted", true)
+			.order("status", { ascending: false });
 
 		if (error) {
+			setLoading(false);
 			console.error("Error fetching projects:", error);
 		} else {
+			setLoading(false);
 			setProjects(convertToCamelCase(data) as Project[]);
 		}
 	};
@@ -37,13 +44,40 @@ function Projects({}: Props) {
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [user]);
-
+	console.log("projects >>> ", projects);
+	useEffect(() => {
+		if (projects.length > 0) {
+			setPercentagesFunded(
+				projects.map((project) => {
+					return (
+						(project?.projectFinancials?.totalAmountRaised /
+							project?.projectFinancials?.finalEstProjectFundRequestTotal) *
+						100
+					);
+				})
+			);
+		}
+	}, [projects]);
+	if (loading)
+		return (
+			<div className='flex flex-col justify-center h-[80vh] items-center'>
+				<Loading message='Loading your projects...' />
+			</div>
+		);
 	return (
-		<div>
-			<h1 className='font-bold text-3xl mx-8 mt-6'>Your Projects</h1>
+		<div className='m-4 w-[80%] mx-auto'>
+			<div className='flex justify-between items-center my-6'>
+				<h1 className='font-bold text-2xl md:text-3xl'>Your Projects</h1>
+				<button
+					onClick={() => router.push("/p/add-project")}
+					className='flex py-2 px-4 md:px-8 border-none rounded-md bg-[var(--cta-one)] text-white cursor-pointer transition-all duration-300 ease-in-out hover:bg-[var(--cta-one-hover)]'
+				>
+					<IoMdAddCircle className='text-2xl mr-2' /> Project
+				</button>
+			</div>
 			<div className='flex flex-wrap'>
 				{projects.length > 0 ? (
-					projects.map((project) => {
+					projects.map((project, index) => {
 						const {
 							id,
 							projectCoordinatorContact,
@@ -61,19 +95,21 @@ function Projects({}: Props) {
 								treeProjects={treeProjects}
 								energyProjects={energyProjects}
 								solarProjects={solarProjects}
+								fetchProjects={fetchProjects}
+								percentageFunded={percentagesFunded[index]}
 							/>
 						);
 					})
 				) : (
-					<div className='flex justify-center items-center w-[80%] mx-auto h-[80vh]'>
-						<p className='text-center mr-4'>
+					<div className='flex flex-col md:flex-row justify-center items-center mx-auto h-[80vh]'>
+						<p className='text-center mb-4 md:mb-0 md:mr-4'>
 							You have no projects yet. Add a new project today!
 						</p>
 						<button
 							onClick={() => router.push("/p/add-project")}
-							className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition-colors cursor-pointer'
+							className='flex bg-[var(--cta-one)] hover:bg-[var(--cta-one-hover)] text-white font-bold py-2 px-4 rounded transition-colors cursor-pointer'
 						>
-							Add project
+							<IoMdAddCircle className='text-2xl mr-2' /> Project
 						</button>
 					</div>
 				)}
