42abd53d48afeb8f366d28969a37f2b5c914d461
ecoappsandautomation <sajan@ecoappsandautomation.com>
2023-12-02 13:56:35 -0700

    Add loading component and update user investments display

--- app/i/portfolio/[id]/page.tsx
+++ app/i/portfolio/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Loading from "@/components/Loading";
 import UserInvestments from "@/components/UserInvestments";
 import Project from "@/components/producer/projects/Project";
 import { useAppSelector } from "@/redux/hooks";
@@ -7,6 +8,7 @@ import withAuth from "@/utils/withAuth";
 import axios from "axios";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { set } from "react-hook-form";
 
 type Props = {};
 
@@ -16,14 +18,23 @@ function PortfolioProject({}: Props) {
 	const user = useAppSelector((state) => state.user);
 	const [project, setProject] = useState<any>(null);
 	const [loading, setLoading] = useState<boolean>(true);
+	const [percentageFunded, setPercentageFunded] = useState<any>(0);
+	const [totalShares, setTotalShares] = useState<any>(0);
+	const [totalAmountInvested, setTotalAmountInvested] = useState<any>(0);
 	const fetchProject = async () => {
 		setLoading(true);
 		await axios
-			.get(`/api/investor/portfolio/project?projectId=${projectId}`)
+			.get(
+				`/api/investor/portfolio/project?projectId=${projectId}&investorId=${user.investorId}`
+			)
 			.then((res) => {
 				setLoading(false);
 				console.log("res.data >>> ", res.data);
 				setProject(convertToCamelCase(res.data.data));
+				setTotalAmountInvested(res.data.totalAmountInvested);
+				setTotalShares(res.data.totalShares);
+				console.log;
+				setPercentageFunded(res.data.percentageFunded);
 			})
 			.catch((err) => {
 				setLoading(false);
@@ -37,13 +48,21 @@ function PortfolioProject({}: Props) {
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [projectId, user]);
+	if (loading) return <Loading />;
 	return (
 		<div className='w-[800px] mx-auto mt-4'>
 			<Project
 				adminMode={false}
 				project={project}
+				percentageFunded={percentageFunded}
 			/>
-			<UserInvestments />
+			<UserInvestments
+				energyInvestments={project?.energyInvestments}
+				treeInvestments={project?.treeInvestments}
+				treeProjectType={project?.treeProjects?.projectType}
+				totalAmountInvested={totalAmountInvested}
+				totalShares={totalShares}
+			/>
 		</div>
 	);
 }
