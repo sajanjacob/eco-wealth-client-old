41a84a112e99e2c3db536d565ace7876af2c04ed
ecoappsandautomation <sajan@ecoappsandautomation.com>
2024-02-12 15:45:35 -0700

    Refactor onboarding routes and insert property data

--- app/api/onboard/producer/route.tsx
+++ app/api/onboard/producer/route.tsx
@@ -1,7 +1,9 @@
+import { BASE_URL } from "@/constants";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import axios from "axios";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import shortid from "shortid";
 
 export async function POST(req: any) {
 	const cookieStore = cookies();
@@ -26,7 +28,6 @@ export async function POST(req: any) {
 				operation_type,
 				current_operations,
 				property_zone_map,
-				address,
 			},
 		])
 		.select();
@@ -41,40 +42,70 @@ export async function POST(req: any) {
 			{ status: 500 }
 		);
 	}
-	if (onboardingData) {
-		const { data: producerData, error: producerError } = await supabase
-			.from("producers")
-			.update({
-				onboarding_complete: true,
-				onboarding_id: onboardingData[0]?.id,
-			})
-			.eq("user_id", userId);
-		if (producerError) {
-			console.error(
-				"Error updating producer onboarding status:",
-				producerError
-			);
-			return NextResponse.json(
-				{ error: producerError.message },
-				{ status: 500 }
-			);
-		}
-		await axios
-			.post(`/api/properties`, {
-				producerId,
-				address,
-			})
-			.then((res) => {
-				console.log("Property created: ", res.data);
 
-				return NextResponse.json(
-					{ message: "Producer onboarding complete." },
-					{ status: 200 }
-				);
-			})
-			.catch((err) => {
-				console.log("Error creating property: ", err);
-				return NextResponse.json({ error: err.message }, { status: 500 });
-			});
+	const { data: producerUpdateData, error: producerError } = await supabase
+		.from("producers")
+		.update({
+			onboarding_complete: true,
+			onboarding_id: onboardingData[0]?.id,
+		})
+		.eq("user_id", userId);
+	if (producerError) {
+		console.error("Error updating producer onboarding status:", producerError);
+		return NextResponse.json({ error: producerError.message }, { status: 500 });
 	}
+	// Insert a new property into the 'producer_properties' table
+	const { data: propertyData, error } = await supabase
+		.from("producer_properties")
+		.insert([
+			{
+				producer_id: producerId,
+				address,
+				is_verified: false,
+			},
+		])
+		.select();
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+
+	// If property data is available, insert a new record into the 'producer_verification_codes' table
+	if (propertyData) {
+		const { data: verificationData, error } = await supabase
+			.from("producer_verification_codes")
+			.insert([
+				{
+					producer_id: producerId,
+					property_id: propertyData[0].id,
+					verification_code: shortid.generate(),
+				},
+			]);
+
+		if (error) {
+			return NextResponse.json({ error: error.message }, { status: 500 });
+		}
+	}
+
+	return NextResponse.json(
+		{ message: "Property inserted successfully", data: propertyData },
+		{ status: 200 }
+	);
+
+	// await axios
+	// 	.post(`${BASE_URL}/api/properties`, {
+	// 		producerId,
+	// 		address,
+	// 	})
+	// 	.then((res) => {
+	// 		console.log("Property created: ", res.data);
+
+	// 		return NextResponse.json(
+	// 			{ message: "Producer onboarding complete." },
+	// 			{ status: 200 }
+	// 		);
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log("Error creating property: ", err);
+	// 		return NextResponse.json({ error: err.message }, { status: 500 });
+	// 	});
 }
--- app/api/onboard/user/route.tsx
+++ app/api/onboard/user/route.tsx
@@ -21,52 +21,50 @@ export async function POST(req: any) {
 		console.error("Error updating user role:", error.message);
 		return NextResponse.json({ error: error.message }, { status: 500 });
 	}
-	if (data) {
-		if (role === "investor") {
-			const { data: investorData, error: investorError } = await supabase
-				.from("investors")
-				.insert([
-					{
-						user_id: userId,
-					},
-				]);
-			const { data: analyticData, error: analyticErr } = await supabase
-				.from("analytics")
-				.insert([
-					{
-						user_id: userId,
-					},
-				]);
-			if (investorError) {
-				console.error("Error inserting investor:", investorError.message);
-				return NextResponse.json(
-					{ error: investorError.message },
-					{ status: 500 }
-				);
-			}
-			if (investorData) {
-				return NextResponse.json({ data: investorData });
-			}
+	if (role === "investor") {
+		const { data: investorData, error: investorError } = await supabase
+			.from("investors")
+			.insert([
+				{
+					user_id: userId,
+				},
+			]);
+		const { data: analyticData, error: analyticErr } = await supabase
+			.from("analytics")
+			.insert([
+				{
+					user_id: userId,
+				},
+			]);
+		if (investorError) {
+			console.error("Error inserting investor:", investorError.message);
+			return NextResponse.json(
+				{ error: investorError.message },
+				{ status: 500 }
+			);
 		}
+		if (investorData) {
+			return NextResponse.json({ data: investorData });
+		}
+	}
 
-		if (role === "producer") {
-			const { data: producerData, error: producerError } = await supabase
-				.from("producers")
-				.insert([
-					{
-						user_id: userId,
-					},
-				]);
-			if (producerError) {
-				console.error("Error inserting producer:", producerError.message);
-				return NextResponse.json(
-					{ error: producerError.message },
-					{ status: 500 }
-				);
-			}
-			if (producerData) {
-				return NextResponse.json({ data: producerData });
-			}
+	if (role === "producer") {
+		const { data: producerData, error: producerError } = await supabase
+			.from("producers")
+			.insert([
+				{
+					user_id: userId,
+				},
+			]);
+		if (producerError) {
+			console.error("Error inserting producer:", producerError.message);
+			return NextResponse.json(
+				{ error: producerError.message },
+				{ status: 500 }
+			);
+		}
+		if (producerData) {
+			return NextResponse.json({ data: producerData });
 		}
 	}
 }
