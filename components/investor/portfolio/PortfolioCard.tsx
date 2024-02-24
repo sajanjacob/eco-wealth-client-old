9721032dff1d929fbe69a4b8e5367091bde9cd43
ecoappsandautomation <sajan@ecoappsandautomation.com>
2024-02-23 18:06:56 -0700

    Fixed banner url on inv portfolio card

--- components/investor/portfolio/PortfolioCard.tsx
+++ components/investor/portfolio/PortfolioCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { LinearProgress } from "@mui/material";
@@ -17,20 +18,38 @@ const PortfolioCard: React.FC<PortfolioCardProps> = ({
 	totalAmountRaised,
 	amountRequested,
 }) => {
+	const { imageUrls } = project;
+	// console.log("project >>> ", project);
+	const [bannerUrl, setBannerUrl] = useState("");
+	useEffect(() => {
+		// look through imageUrls and find the one with isBanner = true and run setBannerUrl to set it to that image
+		if (!imageUrls) return;
+		let bannerFound = false;
+		imageUrls?.forEach((image) => {
+			console.log("image", image);
+			if (image.isBanner) {
+				setBannerUrl(image.url);
+				bannerFound = true;
+			}
+		});
+		// If no banner is found, set the banner to the first image in the array
+		if (!bannerFound && imageUrls.length > 0) {
+			setBannerUrl(imageUrls[0].url);
+		}
+	}, [imageUrls]);
 	if (!project) return <div>loading...</div>;
 	// TODO: Refactor /api/investor/portfolio/route.tsx to return collectiveTotalAmountRaised
 	let percentFunded = 0;
 	if (amountRequested && totalAmountRaised) {
 		percentFunded = (totalAmountRaised / amountRequested) * 100;
 	}
-	// console.log("project >>> ", project);
 
 	return (
 		<div className='lg:w-[308px] w-[408px] bg-transparent rounded-2xl md:mr-2 shadow-md relative mt-8 z-10 h-fit pb-2'>
 			<div>
 				<Link href={`/i/portfolio/${project.id}`}>
 					<Image
-						src={project.imageUrl}
+						src={bannerUrl}
 						width={288}
 						height={150}
 						className='w-full h-48 object-cover rounded-2xl relative'
