02bb3e68746afa1f6ac8e055af616eb2e2b119b4
ecoappsandautomation <sajan@ecoappsandautomation.com>
2024-04-24 15:48:14 -0600

    Refactored signup api for new referral object

--- app/api/signup/route.tsx
+++ app/api/signup/route.tsx
@@ -5,7 +5,9 @@ import { BASE_URL } from "@/constants";
 import { createClient } from "@supabase/supabase-js";
 import sanitizeHtml from "sanitize-html";
 import sanitizeJsonObject from "@/utils/sanitizeJsonObject";
-
+import sanitizeStringArray from "@/utils/sanitizeStringArray";
+import sanitizeArrayOfObjects from "@/utils/sanitizeArrayOfObjects";
+import { deduplicateReferrers } from "@/utils/deduplicateReferrers";
 export async function POST(req: NextRequest) {
 	const cookieStore = cookies();
 	const SUPABASE_URL = process.env.supabase_public_url;
@@ -15,18 +17,23 @@ export async function POST(req: NextRequest) {
 	const {
 		email,
 		password,
-		referrer,
 		referralSource,
-		specificReferral,
 		referrers,
 		referrerIds,
+		specificReferral,
 	} = await req.json();
 
 	const sanitizedEmail = sanitizeHtml(email);
-	const sanitizedReferrers = sanitizeJsonObject(referrers);
 	const sanitizedReferralSource = sanitizeHtml(referralSource);
+	console.log("referrers >>> ", referrers);
+	let sanitizedReferrers = referrers && sanitizeArrayOfObjects(referrers);
+	sanitizedReferrers = deduplicateReferrers(sanitizedReferrers);
 	const sanitizedSpecificReferral = sanitizeHtml(specificReferral);
-	const sanitizedReferrerIds = sanitizeHtml(referrerIds);
+	let sanitizedReferrerIds = referrerIds && sanitizeStringArray(referrerIds);
+	sanitizedReferrerIds = sanitizedReferrerIds.filter(
+		(item: string) => item !== ""
+	); // Remove empty strings within the referrerIds array
+	console.log("sanitizedReferrerIds >>> ", sanitizedReferrerIds);
 
 	console.log("referrer >>> ", referrers);
 	console.log("referralSource >>> ", referralSource);
@@ -44,9 +51,10 @@ export async function POST(req: NextRequest) {
 		console.error("Error signing up:", error.message);
 		return NextResponse.json({ error: error.message }, { status: 500 });
 	}
+	// Update referrer data
 	if (data) {
 		console.log("new account user data >>> ", data);
-		if (!referrer) {
+		if (!referrers || !referrers.length) {
 			return NextResponse.json({ data });
 		}
 		const { data: userData, error: userError } = await supabase
@@ -54,7 +62,7 @@ export async function POST(req: NextRequest) {
 			.update({
 				referrer_ids: sanitizedReferrerIds,
 				referrers: sanitizedReferrers,
-				referral_source: sanitizedReferralSource,
+				referral_source_type: sanitizedReferralSource,
 				specific_referral: sanitizedSpecificReferral,
 			})
 			.eq("id", data.user?.id);
